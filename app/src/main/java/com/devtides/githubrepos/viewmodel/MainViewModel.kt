5ab1f433b94539bc5ec857f6ac3952d686888a00
RachmanForniandi <rachmanforniandi@gmail.com>
2023-02-27 21:33:44 +0700

    flow authentication & test commit repository

--- app/src/main/java/com/devtides/githubrepos/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/devtides/githubrepos/viewmodel/MainViewModel.kt
@@ -1,7 +1,42 @@
 package com.devtides.githubrepos.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devtides.githubrepos.model.GithubService
+import com.devtides.githubrepos.model.GithubToken
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class MainViewModel : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+    val tokenLd = MutableLiveData<String>()
+    val errorLd = MutableLiveData<String>()
+
+    fun getToken(clientId:String, clientSecret:String,code:String){
+        compositeDisposable.add(
+            GithubService.getUnAuthorizedApi().getAuthToken(clientId, clientSecret, code)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<GithubToken>() {
+                    override fun onSuccess(t: GithubToken) {
+                        tokenLd.value = t.accessToken
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        errorLd.value = "Cannot load token"
+                    }
+
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 
 }
